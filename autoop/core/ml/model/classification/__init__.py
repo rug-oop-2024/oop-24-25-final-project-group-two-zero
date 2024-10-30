92f9ddfbeff2917a68f6508b86ccb6742cf5c994
yograph <y.s.m.m.f.farahat@student.rug.nl>
2024-10-30 16:10:41 +0100

    This is for the innit of the classification model

---
+++ autoop/core/ml/model/classification/__init__.py
@@ -0,0 +1,10 @@
+from .LinearRegression import LinearRegressionModel
+from .multiplelinear import MultipleLinearRegression
+from .ridgeregression import RidgeRegression
+
+
+__all__ = [
+    "MultipleLinearRegression",
+    "RidgeRegression",
+    "LinearRegressionModel"
+]
